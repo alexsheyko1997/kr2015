b1b40fa57d6a9d817410ba0472cd3614dcb39c12
alexsheyko1997 <alex17sheyk@gmail.com>
2016-02-03 18:03:19 +0200

    класс в котором запускаеться программа

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+/**
+ * Created by Александр on 03.08.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        MainFrame mainFrame = new MainFrame("HARDCULATOR", new Dimension(350, 450));
+        mainFrame.paint();
+    }
+}
